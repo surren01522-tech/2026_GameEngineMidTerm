74b0fffda0822b679bb33187bcbde09af14473bc
surren01522-tech <surren01522@gmail.com>
2026-04-29 15:25:57 +0900

    9강

--- MidTermProject/Assets/Scripts/ScoreTest.cs
+++ MidTermProject/Assets/Scripts/ScoreTest.cs
@@ -5,12 +5,18 @@ public class ScoreTest : MonoBehaviour
 {
     public TextMeshProUGUI stage1;
     public TextMeshProUGUI stage2;
+    public TextMeshProUGUI stage3;
+    public TextMeshProUGUI stage4;
+    public TextMeshProUGUI stage5;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         stage1.text = "STAGE 1 : " + HighScore.Load(1).ToString();
         stage1.text = "STAGE 2 : " + HighScore.Load(2).ToString();
+        stage1.text = "STAGE 3 : " + HighScore.Load(3).ToString();
+        stage1.text = "STAGE 4 : " + HighScore.Load(4).ToString();
+        stage1.text = "STAGE 5 : " + HighScore.Load(5).ToString();
     }
 
     // Update is called once per frame
