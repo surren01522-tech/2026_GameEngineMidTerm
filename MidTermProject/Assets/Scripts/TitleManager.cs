0fef11ae635c559684b3df274bc8e759ad5149ab
surren01522-tech <surren01522@gmail.com>
2026-04-29 15:47:34 +0900

    9강

--- MidTermProject/Assets/Scripts/TitleManager.cs
+++ MidTermProject/Assets/Scripts/TitleManager.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class TitleManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject ScorePanel;
+    private bool ScorePanelActive = false;
 
-    // Update is called once per frame
-    void Update()
+    public void ScorePanelToggle()
     {
-        
+        ScorePanelActive = !ScorePanelActive;
+        ScorePanel.SetActive(ScorePanelActive);
     }
 }
